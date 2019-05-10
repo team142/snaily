b685711e849fb8c8734411636fea03df5d54d7ec
Just1689 <Just1689@gmail.com>
2019-05-10 17:43:57 +0200

    PG

---
+++ start-pg.bat
@@ -0,0 +1,2 @@
+docker volume create snaily-db-data
+docker service create --name spg --publish 5000:5432 --limit-memory 32M --mount type=volume,source=snaily-db-data,destination=/var/lib/postgresql/data postgres
