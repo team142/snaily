2ea5f899aa63b3727c21a7d54a854736be7157f6
Justin Tamblyn <just1689@gmail.com>
2019-05-13 12:57:07 +0200

    Clean up

--- snaily-web/src/app/model/state/userState.ts
+++ snaily-web/src/app/model/state/userState.ts
@@ -1,17 +1,17 @@
-
 export class UserState {
 
-    private static loggedIn: boolean = false;
+  private static loggedIn = false;
 
-    public static isLoggedIn(): boolean {
-        return this.loggedIn;
-    }
+  public static isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
 
-    public static login() {
-        this.loggedIn = true;
-    }
+  public static login() {
+    this.loggedIn = true;
+  }
 
-    public static logout() {
-        this.loggedIn = false;
-    }
+  public static logout() {
+    this.loggedIn = false;
+  }
+
 }
