b03b974d223e1f09be9d68af08889150c2ae9df0
Just1689 <Just1689@gmail.com>
2019-05-14 20:49:27 +0200

    Docker container and start application in container

--- application.go
+++ application.go
@@ -12,6 +12,7 @@ import (
 
 var addr = flag.String("address", ":8080", "")
 var addrToProxy = flag.String("proxy", "http://localhost:4200", "The url of the angular app to reverse proxy")
+var container = flag.Bool("container", false, "The url of the angular app to reverse proxy")
 
 func main() {
 	flag.Parse()
@@ -22,7 +23,13 @@ func main() {
 	router.PathPrefix("/api").HandlerFunc(api.HandleIncoming)
 
 	//Handles everything else
-	router.PathPrefix("/").HandlerFunc(buildHomeRouter())
+	if *container {
+		//Serve from web folder
+		// TODO: host locally
+	} else {
+		//When running locally - reverse proxy to node js server
+		router.PathPrefix("/").HandlerFunc(buildHomeRouter())
+	}
 
 	//The server
 	http.Handle("/", router)
