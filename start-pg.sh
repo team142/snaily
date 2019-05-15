686954e2b898b065e2914a387c6bdeed8da5bc9c
Justin Tamblyn <just1689@gmail.com>
2019-05-15 08:49:27 +0200

    Add to my-services network

--- start-pg.sh
+++ start-pg.sh
@@ -2,3 +2,4 @@
 
 docker volume create snaily-db-data
 docker service create --name spg --publish 5000:5432 --limit-memory 32M --mount type=volume,source=snaily-db-data,destination=/var/lib/postgresql/data -e POSTGRES_PASSWORD=snaily -e POSTGRES_DB=madast -e POSTGRES_USER=snaily postgres
+docker service update spg --network-add my-services
