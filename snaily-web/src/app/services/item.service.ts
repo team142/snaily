fe736c0c330d4030a037d4761ab1a2af7e71b30d
Just1689 <Just1689@gmail.com>
2019-05-27 16:43:54 +0200

    Send key

--- snaily-web/src/app/services/item.service.ts
+++ snaily-web/src/app/services/item.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {ItemV1} from '../model/item-v1';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {MyItemsRequestV1} from '../model/my-items-request-v1';
+import {UserState} from '../model/state/userState';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,14 @@ import {MyItemsRequestV1} from '../model/my-items-request-v1';
 export class ItemService {
 
   constructor(private http: HttpClient) {
+
   }
 
   public post(item: ItemV1, win: Function, fail: Function): void {
-    this.http.post(window.location.origin + environment.urlNewItemV1, item)
+    this.http.post(window.location.origin + environment.urlNewItemV1, item, {
+        headers: new HttpHeaders().append('key', UserState.getKey())
+      }
+    )
       .toPromise()
       .then((result) => {
         win(result);
@@ -25,8 +30,10 @@ export class ItemService {
   }
 
   public getMyItems(item: MyItemsRequestV1, win: Function, fail: Function): void {
-    this.http.post(window.location.origin + environment.urlMyItemsV1, item)
-      .toPromise()
+    this.http.post(window.location.origin + environment.urlMyItemsV1, item, {
+        headers: new HttpHeaders().append('key', UserState.getKey())
+      }
+    ).toPromise()
       .then((result) => {
         win(result);
       })
@@ -36,8 +43,10 @@ export class ItemService {
   }
 
   public getItem(item: ItemV1, win: Function, fail: Function): void {
-    this.http.post(window.location.origin + environment.urlGetItemV1, item)
-      .toPromise()
+    this.http.post(window.location.origin + environment.urlGetItemV1, item, {
+        headers: new HttpHeaders().append('key', UserState.getKey())
+      }
+    ).toPromise()
       .then((result) => {
         win(result);
       })
